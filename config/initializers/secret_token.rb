5c481fc317e5736abe26e6ac14b204bdcfde08e7
Abhi Ramesh <abhiramesh@gmail.com>
2013-04-25 12:54:10 -0700

    Create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mortgage3::Application.config.secret_token = '902d14a8b7120199ee840aeb1ccb1cd0a07e84fb2687fd52d6416640034fdac5039f501ffb53d97e291721ad4a747f2c0d4ac1b577a546a9d21c7e341b846ebb'
