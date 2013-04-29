f14be39f748ece9c986743c0b804d2934e20a412
Abhi Ramesh <abhiramesh@gmail.com>
2013-04-29 15:13:29 -0700

    user sort

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.all.sort
 
     respond_to do |format|
       format.html # index.html.erb
